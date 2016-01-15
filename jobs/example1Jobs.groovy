a1a0f2eb1557e92fa25f2d2c9312df7f97827991
daniloalvim <daniloalvim@gmail.com>
2016-01-15 11:27:17 -0200

    Update groovy
    asfas

--- jobs/example1Jobs.groovy
+++ jobs/example1Jobs.groovy
@@ -28,3 +28,9 @@ job("$basePath/grails-example-deploy") {
         shell 'scp war file; restart...'
     }
 }
+
+job('joao-feijao'){
+    steps{
+        shell 'echo Meu Primeiro build gerado automaticamente:úmero $BUILD_NUMBER'
+    }
+}
